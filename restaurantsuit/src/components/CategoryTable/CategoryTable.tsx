7a158969a8ef30c5084a609ccb5c96819662ba45
gabeFrancisco <gabegit2@gmail.com>
2023-03-25 00:44:19 -0200

    Created inputs and formik for CategoryField

--- restaurantsuit/src/components/CategoryTable/CategoryTable.tsx
+++ restaurantsuit/src/components/CategoryTable/CategoryTable.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import CategoryRow from "../tableRows/CategoryRow/CategoryRow";
+import CategoryRow from "../TableRows/CategoryRow/CategoryRow";
 import { fetchAllCategories } from "../../store/features/categoriesSlice";
 import { useAppDispatch, useAppSelector } from "../../store/store";
 
 import "./CategoryTable.css";
+import CategoryField from "../Fields/CategoryField/CategoryField";
 
 export default function CategoryTable() {
   const categories = useAppSelector((state) => state.categories.categoryList);
@@ -25,6 +26,7 @@ export default function CategoryTable() {
           </tr>
         </thead>
         <tbody>
+          
           {categories.map((cat) => (
             <CategoryRow
               id={cat.id}
@@ -33,6 +35,22 @@ export default function CategoryTable() {
               productsQty={7}
             />
           ))}
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Cor</th>
+            <th>Qte. Produtos</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <CategoryField id={7}
+              categoryName={'cat.name'}
+              color={'cat.color'}
+              productsQty={7}/>
         </tbody>
       </table>
     </div>
